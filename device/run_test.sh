068e70b945129d682e5a2b38cb7b572d3db3b885
Konrad <konradha@yahoo.com>
2025-02-17 15:10:06 +0100

    parallelizing trajectory generation using openmp

--- device/run_test.sh
+++ device/run_test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-#SBATCH --job-name=nlse_test  
+#SBATCH --job-name=nlse_parallel_test  
 #SBATCH --time=00:12:00         
 #SBATCH --mem-per-cpu=4G                          
 #SBATCH --gpus=1    
-#SBATCH --output=test_nlse_%j.out     
-#SBATCH --error=test_nlse_%j.err       
+#SBATCH --output=test_nlse_parallel_%j.out     
+#SBATCH --error=test_nlse_parallel_%j.err       
+#SBATCH --cpus-per-task=8
 
 
 #cmd="./to_lanczos_complex_test"
 #nvprof ${cmd}
 #compute-sanitizer ./to_matfunc_real_test
-./to_nlse_driver
+./to_nlse_driver_par
