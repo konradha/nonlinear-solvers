9274bd27c89b9dc7ba9b150b1de2e4497b30194c
Konrad <konradha@yahoo.com>
2025-02-17 14:05:09 +0100

    add working NLSE integrator test

---
+++ device/nlse_solver_dev.hpp
@@ -0,0 +1,135 @@
+#ifndef NLSE_SOLVER_DEV_HPP
+#define NLSE_SOLVER_DEV_HPP
+
+#include "matfunc_complex.hpp"
+#include "pragmas.hpp"
+#include <thrust/complex.h>
+
+__global__ void density(thrust::complex<double> *out, 
+                                     const thrust::complex<double> *in,
+                                     const uint32_t nx, const uint32_t ny) {
+    const int x = blockIdx.x * blockDim.x + threadIdx.x;
+    const int y = blockIdx.y * blockDim.y + threadIdx.y;
+    if (x < nx && y < ny) {
+        const int idx = y * nx + x;
+        const auto val = in[idx];
+        out[idx] = thrust::abs(val) * thrust::abs(val);
+    }
+}
+
+__global__ void nonlin_part(thrust::complex<double> *out,
+                                      const thrust::complex<double> *in,
+                                      const thrust::complex<double> *density,
+                                      const thrust::complex<double> tau,
+                                      const uint32_t nx, const uint32_t ny) {
+    const int x = blockIdx.x * blockDim.x + threadIdx.x;
+    const int y = blockIdx.y * blockDim.y + threadIdx.y;
+    if (x < nx && y < ny) {
+        const int idx = y * nx + x;
+        out[idx] = thrust::exp(tau * density[idx]) * in[idx];
+    }
+}
+
+class NLSESolverDevice {
+public:
+    struct Parameters {
+        uint32_t block_size;
+        uint32_t num_snapshots;
+        uint32_t snapshot_freq;
+        uint32_t krylov_dim;
+        Parameters(uint32_t ns = 100, uint32_t freq = 5, uint32_t m = 10)
+            : block_size(256), num_snapshots(ns), snapshot_freq(freq),
+              krylov_dim(m) {}
+    };
+
+    NLSESolverDevice(const Eigen::SparseMatrix<std::complex<double>> &L,
+                     const std::complex<double> *host_u0,
+                     const Parameters &params = Parameters())
+        : n_(L.rows()), current_snapshot_(0), params_(params) {
+        
+        cudaMalloc(&d_u_, n_ * sizeof(thrust::complex<double>));
+        cudaMalloc(&d_buf_, n_ * sizeof(thrust::complex<double>));
+        cudaMalloc(&d_density_, n_ * sizeof(thrust::complex<double>));
+        cudaMalloc(&d_u_trajectory_, n_ * params_.num_snapshots * sizeof(thrust::complex<double>));
+        
+        cudaMemcpy(d_u_, host_u0, n_ * sizeof(thrust::complex<double>), 
+                  cudaMemcpyHostToDevice);
+
+        matfunc_ = new MatrixFunctionApplicatorComplex(L, n_, params_.krylov_dim, 
+                                                      L.nonZeros());
+
+        nx_ = sqrt(n_);
+        ny_ = nx_;
+        uint32_t threads = 256;
+        block_dim_ = dim3(threads / 16, threads / 16);
+        grid_dim_ = dim3((nx_ + block_dim_.x - 1) / block_dim_.x,
+                        (ny_ + block_dim_.y - 1) / block_dim_.y);
+
+        cudaDeviceSynchronize();
+        cudaError_t err = cudaGetLastError();
+        if (err != cudaSuccess) {
+            throw std::runtime_error(cudaGetErrorString(err));
+        }
+        
+        store_snapshot(0);
+    }
+
+    ~NLSESolverDevice() {
+        delete matfunc_;
+        cudaFree(d_u_);
+        cudaFree(d_buf_);
+        cudaFree(d_density_);
+        cudaFree(d_u_trajectory_);
+    }
+
+    void step(const std::complex<double> tau, const uint32_t step_number) {
+        density<<<grid_dim_, block_dim_>>>(d_density_, d_u_, 
+                                                         nx_, ny_); 
+        nonlin_part<<<grid_dim_, block_dim_>>>(d_buf_, d_u_, 
+                                                          d_density_,
+                                                          -.5 * tau, nx_, ny_);
+        matfunc_->apply(d_u_, d_buf_, -tau);
+	
+        density<<<grid_dim_, block_dim_>>>(d_density_, d_u_, nx_, ny_);
+        nonlin_part<<<grid_dim_, block_dim_>>>(d_u_, d_u_, 
+                                                          d_density_,
+                                                          -.5 * tau, nx_, ny_);
+        cudaError_t err = cudaGetLastError();
+        if (err != cudaSuccess) {
+            throw std::runtime_error(cudaGetErrorString(err));
+        }
+
+        if (step_number % params_.snapshot_freq == 0) {
+            store_snapshot(step_number);
+        }
+    }
+
+    void transfer_snapshots(std::complex<double> *dst) {
+        cudaMemcpy(dst, d_u_trajectory_,
+                  params_.num_snapshots * n_ * sizeof(thrust::complex<double>),
+                  cudaMemcpyDeviceToHost);
+    }
+
+private:
+    void store_snapshot(const uint32_t step_number) {
+        cudaMemcpy(d_u_trajectory_ + current_snapshot_ * n_, d_u_,
+                  n_ * sizeof(thrust::complex<double>), 
+                  cudaMemcpyDeviceToDevice);
+        current_snapshot_++;
+    }
+
+    MatrixFunctionApplicatorComplex *matfunc_;
+    thrust::complex<double> *d_u_;
+    thrust::complex<double> *d_buf_;
+    thrust::complex<double> *d_density_;
+    thrust::complex<double> *d_u_trajectory_;
+
+    uint32_t nx_, ny_;
+    uint32_t current_snapshot_;
+    uint32_t n_;
+    dim3 grid_dim_;
+    dim3 block_dim_;
+    Parameters params_;
+};
+
+#endif
--- device/run_test.sh
+++ device/run_test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=matfunc_complex_test  
+#SBATCH --job-name=nlse_test  
 #SBATCH --time=00:12:00         
 #SBATCH --mem-per-cpu=4G                          
 #SBATCH --gpus=1    
-#SBATCH --output=matfunc_complex_%j.out     
-#SBATCH --error=matfunc_complex_%j.err       
+#SBATCH --output=test_nlse_%j.out     
+#SBATCH --error=test_nlse_%j.err       
 
 
 #cmd="./to_lanczos_complex_test"
 #nvprof ${cmd}
 #compute-sanitizer ./to_matfunc_real_test
-compute-sanitizer ./to_matfunc_complex_test
+./to_nlse_driver
