21be4c1d65cafb16a99ab7c5e11a2a43d8d9b662
Konrad <konradha@yahoo.com>
2025-02-28 17:58:28 +0100

    explored topping out of personal dir on Euler; needs optimizing and downscaling

--- nlsolvers/scripts/launch_sge_single.sh
+++ nlsolvers/scripts/launch_sge_single.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
-#SBATCH --job-name=sg_solver
-#SBATCH --time=00:05:00
+#SBATCH --job-name=sg_solver_dev
+#SBATCH --time=00:10:00
 #SBATCH --mem-per-cpu=8G
 #SBATCH --gpus=1
-#SBATCH --output=/cluster/scratch/konradha/sg_solver_%j.out
-#SBATCH --error=/cluster/scratch/konradha/sg_solver_%j.err
+#SBATCH --output=logs/sg_solver_dev_%j.out
+#SBATCH --error=logs/sg_solver_dev_%j.err
 #SBATCH --ntasks=1
 #SBATCH --partition=gpu
 
+# This script needs to be launched from the nlsolvers/scripts dir.
+# The directory tree is hardcoded to ease launches.
+mkdir ${SCRATCH}/sge_dev_${SLURM_JOB_ID}
+mkdir -p ../data
+mkdir -p ../data/sge_single_dev
+
+# Some directories are created when running this job:
+# - the logs dir (if it does not exist yet)
+# - the output dir in scratch
+# - TODO: downsampling infrastructure before writing into hdf5
+
+# Loading modules (done to be safe) carries additional overhead.
+# Eyeballing it to about a minute.
 module load stack/2024-06 cuda/12.1.1 python/3.11.6 eigen
 module load python_cuda/3.11.6
 module load gcc/12.2.0 
-module load cmake/3.27.7
 
-mkdir ${SCRATCH}/sge_dev_${SLURM_JOB_ID}
+# Pre- and postprocessing have considerable overhead. Hence 300² == 3-5 seconds for m=10 Krylov
+# subspace iterations is too low of a bound! To be safe, let's double it.
 python sge_single.py --exe=../build/bin/sg_single_dev\
 	--nx 256 \
     	--ny 256 \
@@ -22,6 +35,8 @@ python sge_single.py --exe=../build/bin/sg_single_dev\
     	--Ly 10.0 \
     	--T 10.0 \
     	--nt 1000 \
-    	--snapshots 100 \
-    	--num-runs 10 \
+    	--snapshots 101 \
+    	--num-runs 50 \
     	--output-dir ${SCRATCH}/sge_dev_${SLURM_JOB_ID}
+
+cp ${SCRATCH}/sge_dev_${SLURM_JOB_ID}/hdf5/* ../data/sge_single_dev/
