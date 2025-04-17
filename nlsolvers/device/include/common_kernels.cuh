f029b0c4229fe1fcf7cfb7fdcf4f06c0c217066a
Konrad Handrick <konradha@yahoo.com>
2025-04-17 10:32:34 +0200

    add missing kernels

---
+++ nlsolvers/device/include/common_kernels.cuh
@@ -0,0 +1,15 @@
+#ifndef COMMON_KERNELS__CUH
+#define COMMON_KERNELS__CUH
+
+#include <cuda_runtime.h>
+
+__global__ void velocity_kernel(double *v,
+        const double *u, const double *u_past, 
+        const double dt, const uint32_t n) {
+  const uint32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (idx < n) {
+    v[idx] = (u[idx] - u_past[idx]) / dt;
+  }
+}
+
+#endif
