3fe98e96563c9642aaaf94269a8e5aecb0004677
Konrad <konradha@yahoo.com>
2025-04-28 01:08:56 +0200

    split comparison scripts

---
+++ nlsolvers/integrator_comparison_nlse_dev.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name=integrator_comparison
+#SBATCH --time=00:59:00
+#SBATCH --mem-per-cpu=32G
+#SBATCH --mail-type=END
+#SBATCH --mail-user=konradha@yahoo.com
+#SBATCH --gpus=1
+#SBATCH --output=logs/integrator_comparison_%j.out
+#SBATCH --error=logs/integrator_comparison_%j.err
+#SBATCH --ntasks=4
+#SBATCH --partition=gpu
+
+module load stack/2024-06 cuda/12.1.1 python/3.11.6 eigen
+module load python_cuda/3.11.6
+module load gcc/12.2.0 
+module load ffmpeg
+
+
+python scripts_nlse/compare_integrators_nlse_3d.py \
+       	--exe1 build/bin/nlse_3d_dev \
+	--exe2 build/bin/nlse_cubic_host_3d \
+       	--name1 SS2-dev --name2 SS2-host\
+       	--nx 180 --ny 180 --nz 180\
+       	--Lx 3. --Ly 3. --Lz 3.\
+       	--T .5 --nt 500 \
+	--output-dir ${SCRATCH}/compare_ss2_host_dev_nlse_${SLURM_JOB_ID}
--- nlsolvers/kill_script.sh
+++ nlsolvers/kill_script.sh
@@ -3,3 +3,6 @@
 while true; do
 	scancel $(squeue | cut -c 11- | cut -c -10 | tail -n 1)
 done
+
+#inspect:
+# (watch -n1) srun --jobid=JOBID ps -o pid,user,pcpu,pmem,time,cmd -u $USER
