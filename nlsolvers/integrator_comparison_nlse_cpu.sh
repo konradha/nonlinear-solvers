770c3f4ecd091d02eb876e021f15840d2e2b53c3
Konrad <konradha@yahoo.com>
2025-04-28 11:23:54 +0200

    small changes

--- nlsolvers/integrator_comparison_nlse_cpu.sh
+++ nlsolvers/integrator_comparison_nlse_cpu.sh
@@ -3,12 +3,13 @@
 #SBATCH --job-name=integrator_comparison
 #SBATCH --time=02:59:00
 #SBATCH --mem-per-cpu=32G
-#SBATCH --mail-type=END
 #SBATCH --mail-user=konradha@yahoo.com
+#SBATCH --mail-type=END,FAIL
 
 #SBATCH --output=logs/integrator_comparison_%j.out
 #SBATCH --error=logs/integrator_comparison_%j.err
 #SBATCH --ntasks=1
+#SBATCH --nice=0
 
 module load stack/2024-06 cuda/12.1.1 python/3.11.6 eigen
 module load python_cuda/3.11.6
@@ -20,7 +21,7 @@ python scripts_nlse/compare_integrators_nlse_3d.py \
        	--exe1 build/bin/nlse_cubic_host_3d \
 	--exe2 build/bin/nlse_cubic_host_gautschi_3d\
        	--name1 SS2-host --name2 sEWI-host\
-       	--nx 100 --ny 100 --nz 100\
+       	--nx 32 --ny 32 --nz 32\
        	--Lx 3. --Ly 3. --Lz 3.\
        	--T 1. --nt 100 --num-snapshots 100 \
 	--output-dir ${SCRATCH}/compare_ss2_sewi_host_nlse_${SLURM_JOB_ID}
